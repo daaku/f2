07da50cc31d9cd75ae9860f3bcece81384d77895
Naitik Shah <n@daaku.org>
2020-06-09 16:32:49 +0400

    remove custom line length and fmt

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,7 @@ impl Account {
         hmac.input(&counter.to_be_bytes());
         let code = hmac.result().code();
         let offset = (code[code.len() - 1] & 0xf) as usize;
-        let code = u32::from_be_bytes(code[offset..offset + 4].try_into()?)
-            & 0x7fff_ffff;
+        let code = u32::from_be_bytes(code[offset..offset + 4].try_into()?) & 0x7fff_ffff;
         let code = u64::from(code) % 10_u64.pow(self.digits.try_into()?);
         Ok(format!("{:06}", code))
     }
@@ -61,9 +60,8 @@ where
     D: serde::Deserializer<'de>,
 {
     use serde::de::Error;
-    String::deserialize(deserializer).and_then(|s| {
-        base64::decode(&s).map_err(|err| Error::custom(err.to_string()))
-    })
+    String::deserialize(deserializer)
+        .and_then(|s| base64::decode(&s).map_err(|err| Error::custom(err.to_string())))
 }
 
 #[derive(StructOpt, Debug)]
@@ -88,8 +86,7 @@ enum Command {
 }
 
 lazy_static! {
-    static ref DEFAULT_FILE: PathBuf =
-        dirs::home_dir().unwrap_or_default().join(".f2");
+    static ref DEFAULT_FILE: PathBuf = dirs::home_dir().unwrap_or_default().join(".f2");
     static ref SCRYPT_PARAMS: ScryptParams =
         ScryptParams::new(20, 8, 1).expect("valid scrypt params");
 }
@@ -149,9 +146,7 @@ impl App {
                 if err.kind() == std::io::ErrorKind::NotFound {
                     match mode {
                         Load::Optional => return Ok(()),
-                        Load::Required => {
-                            return Err(static_err("No data file found."))
-                        }
+                        Load::Required => return Err(static_err("No data file found.")),
                     }
                 } else {
                     return Err(err.into());
@@ -160,17 +155,15 @@ impl App {
         };
         let reader = BufReader::new(file);
         let outer: Outer = serde_json::from_reader(reader)?;
-        self.passwd =
-            Some(rpassword::read_password_from_tty(Some("Password: "))?);
+        self.passwd = Some(rpassword::read_password_from_tty(Some("Password: "))?);
         let key = scrypt_key(
             self.passwd.as_ref().expect("password to be set").as_bytes(),
             &outer.passwd_salt,
         )?;
-        let sb = SecretBox::new(&key, CipherType::Salsa20)
-            .expect("SecretBox creation");
-        let message = sb.easy_unseal(&outer.message).ok_or_else(|| {
-            static_err("Decryption failed: invalid password or corrupt file.")
-        })?;
+        let sb = SecretBox::new(&key, CipherType::Salsa20).expect("SecretBox creation");
+        let message = sb
+            .easy_unseal(&outer.message)
+            .ok_or_else(|| static_err("Decryption failed: invalid password or corrupt file."))?;
         self.accounts = serde_json::from_slice(&message)?;
         Ok(())
     }
@@ -182,8 +175,7 @@ impl App {
             self.passwd.as_ref().expect("password to be set").as_bytes(),
             &passwd_salt,
         )?;
-        let sb = SecretBox::new(&key, CipherType::Salsa20)
-            .expect("valid SecretBox creation");
+        let sb = SecretBox::new(&key, CipherType::Salsa20).expect("valid SecretBox creation");
         let message = sb.easy_seal(&serde_json::to_vec(&self.accounts)?);
         let message = serde_json::to_vec(&Outer {
             passwd_salt,
@@ -197,9 +189,7 @@ impl App {
         self.load(Load::Required)?;
         let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() / 30;
         let mut table = Table::new();
-        table.set_format(
-            *prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE,
-        );
+        table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
         for a in &self.accounts {
             table.add_row(row![
                 b->a.name,
@@ -217,10 +207,8 @@ impl App {
         if self.passwd.is_none() {
             println!("Adding first account. Please configure your password.");
             println!("Use as long a password as possible.");
-            let passwd =
-                rpassword::read_password_from_tty(Some("New Password: "))?;
-            let confirm =
-                rpassword::read_password_from_tty(Some("Confirm Password: "))?;
+            let passwd = rpassword::read_password_from_tty(Some("New Password: "))?;
+            let confirm = rpassword::read_password_from_tty(Some("Confirm Password: "))?;
             if passwd != confirm {
                 return Err(static_err("Password do not match!"));
             }
@@ -233,16 +221,13 @@ impl App {
                 break;
             }
             let digits = {
-                let digits =
-                    rprompt::prompt_reply_stdout("Digits (default 6): ")?;
+                let digits = rprompt::prompt_reply_stdout("Digits (default 6): ")?;
                 if digits.is_empty() {
                     6
                 } else {
                     let digits = digits.parse()?;
                     if digits != 6 || digits != 7 || digits != 8 {
-                        return Err(static_err(
-                            "Invalid digits: must be one of 6, 7 or 8.",
-                        ));
+                        return Err(static_err("Invalid digits: must be one of 6, 7 or 8."));
                     }
                     digits
                 }
@@ -251,9 +236,7 @@ impl App {
                 base32::Alphabet::RFC4648 { padding: false },
                 &rprompt::prompt_reply_stdout("Key: ")?.to_ascii_uppercase(),
             )
-            .ok_or_else(|| {
-                static_err("Invalid key: a valid key must be base32 encoded.")
-            })?;
+            .ok_or_else(|| static_err("Invalid key: a valid key must be base32 encoded."))?;
             println!("Added {}.", name);
             self.accounts.push(Account { name, digits, key });
         }
@@ -270,9 +253,7 @@ impl App {
             .accounts
             .iter()
             .position(|a| a.name == name)
-            .ok_or_else(|| {
-                static_err("Account with given name was not found.")
-            })?;
+            .ok_or_else(|| static_err("Account with given name was not found."))?;
         println!("Removed {}.", name);
         self.accounts.remove(index);
         self.save()
@@ -280,22 +261,16 @@ impl App {
 
     fn command_passwd(&mut self) -> Result<()> {
         self.load(Load::Required)?;
-        self.passwd =
-            Some(rpassword::read_password_from_tty(Some("New Password: "))?);
+        self.passwd = Some(rpassword::read_password_from_tty(Some("New Password: "))?);
         self.save()
     }
 
     fn command_raw(&mut self) -> Result<()> {
         self.load(Load::Required)?;
         let mut table = Table::new();
-        table.set_format(
-            *prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE,
-        );
+        table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
         for a in &self.accounts {
-            let key = base32::encode(
-                base32::Alphabet::RFC4648 { padding: false },
-                &a.key,
-            );
+            let key = base32::encode(base32::Alphabet::RFC4648 { padding: false }, &a.key);
             table.add_row(row![
                 b->a.name,
                 a.digits,
